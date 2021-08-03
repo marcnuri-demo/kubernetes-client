9b8930ed7babc4208355c6a0c1bba061584106ec
Marc Nuri <marc@marcnuri.com>
2021-08-03 09:54:37 +0200

    refactor: Update Karaf example

--- karaf/src/main/java/com/marcnuri/demo/kubernetesclient/karaf/KarafApplication.java
+++ karaf/src/main/java/com/marcnuri/demo/kubernetesclient/karaf/KarafApplication.java
@@ -3,7 +3,6 @@ package com.marcnuri.demo.kubernetesclient.karaf;
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.api.model.ObjectMeta;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
-import io.fabric8.kubernetes.client.KubernetesClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,17 +12,18 @@ public class KarafApplication {
 
   public void init() {
     log.info("Karaf Application has started");
-    final KubernetesClient kubernetesClient = new DefaultKubernetesClient();
-    log.info("Available nodes:");
-    kubernetesClient.nodes().list().getItems().stream()
-      .map(HasMetadata::getMetadata)
-      .map(ObjectMeta::getName)
-      .forEach(log::info);
-    log.info("Pods:");
-    kubernetesClient.pods().list().getItems().stream()
-      .forEach(pod -> log.info("{} - {}", pod.getMetadata().getNamespace(), pod.getMetadata().getName()));
-    kubernetesClient.load(KarafApplication.class.getResourceAsStream("/pod.yml"));
-    kubernetesClient.load(KarafApplication.class.getResourceAsStream("/deployment.yml"));
+    try (final var kubernetesClient = new DefaultKubernetesClient()) {
+      log.info("Available nodes:");
+      kubernetesClient.nodes().list().getItems().stream()
+        .map(HasMetadata::getMetadata)
+        .map(ObjectMeta::getName)
+        .forEach(log::info);
+      log.info("Pods:");
+      kubernetesClient.pods().list().getItems().forEach(pod ->
+        log.info("{} - {}", pod.getMetadata().getNamespace(), pod.getMetadata().getName()));
+      kubernetesClient.load(KarafApplication.class.getResourceAsStream("/pod.yml"));
+      kubernetesClient.load(KarafApplication.class.getResourceAsStream("/deployment.yml"));
+    }
   }
 
   public void destroy() {
